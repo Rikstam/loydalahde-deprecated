6108f494be32aff2ca831ae7b05652a2021a8e09
Riku Kestilä <rikukestila@gmail.com>
2016-02-21 13:08:33 +0200

    add migration for cities table

---
+++ database/migrations/2016_01_03_205706_create_cities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+//use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Phaza\LaravelPostgis\Schema\Blueprint;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->point('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cities');
+    }
+}
