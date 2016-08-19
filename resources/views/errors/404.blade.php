3982d2b439c9f961f2e308c79bbf86ee8719db15
Riku Kestilä <rikukestila@gmail.com>
2016-08-19 11:20:28 +0300

    add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,29 @@
+@extends('app')
+@section('content')
+    <div id="top-banner">
+        <div class="container">
+            <div id="call-to-action">
+                <a name="haku"></a>
+                <div class="slogan">
+                    <h1>404 Sisältöä ei löydy</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <article class="page col-xs-12 col-md-8 col-md-offset-2">
+
+
+
+
+
+
+
+
+            </article>
+
+        </div>
+    </div>
+
+@endsection
