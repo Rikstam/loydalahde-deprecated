a4afcddc492fdbf3e2572e7a78e7e6f19b568e7e
Riku Kestilä <rikukestila@gmail.com>
2017-01-29 17:04:53 +0200

    fix admin logout to POST request

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -46,7 +46,17 @@
                 <ul class="nav navbar-nav">
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="/logout">Logout</a></li>
+                    <li>
+                        <a href="{{ url('/logout') }}"
+                           onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+
+                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </li>
                 </ul>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
