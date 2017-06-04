f29ae14e60dc6c9123041cfb7f40a5cfd1597e80
Riku Kestilä <rikukestila@gmail.com>
2017-06-04 12:19:50 +0300

    register debugbar locally only

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         if ($this->app->environment('local')) {
-            $this->app->register('Barryvdh\Debugbar\ServiceProvider');
+            $this->app->register(Barryvdh\Debugbar\ServiceProvider::class);
         }
 
         if ($this->app->environment('local', 'testing')) {
